305da6009ca84db6cd80a65f2586ebbd4708346f
Wesley L. Handy <wesley.handy@cbn.org>
2020-08-08 10:40:07 -0400

    fetch update

--- grabatix-server/utils/fetch.js
+++ grabatix-server/utils/fetch.js
@@ -1,49 +1,50 @@
-const fetch = require('node-fetch');
+/* eslint-disable prefer-promise-reject-errors */
+const fetch = require(`node-fetch`)
 
 async function callApi(uri, options = {}) {
-  let data;
+  let data
   try {
-    data = await loadData(uri, options);
-    return data;
+    data = await loadData(uri, options)
+    return data
   } catch (err) {
-    const { body, status } = err;
-    const error = new Error(body ? body : err);
-    error.status = status ? status : 500;
-    error.body = body;
-    console.error({ callApiFetchErr: error });
-    throw error;
+    const { body, status } = err
+    const error = new Error(body ? body : err)
+    error.status = status ? status : 500
+    error.body = body
+    console.error({ callApiFetchErr: error })
+    throw error
   }
 }
 
 async function loadData(uri, options = {}) {
-  const response = await fetch(uri, options);
-  const contentType = response.headers.get('content-type');
-  const { status } = response;
-  console.log({ status, contentType });
+  const response = await fetch(uri, options)
+  const contentType = response.headers.get(`content-type`)
+  const { status } = response
+  console.log({ status, contentType })
   if (status >= 200 && status < 300) {
-    if (contentType && contentType.includes('application/json')) {
-      return response.json();
+    if (contentType && contentType.includes(`application/json`)) {
+      return response.json()
     } else {
-      return response.text();
+      return response.text()
     }
   } else {
     return getErrorBody(response, contentType).then((body) => {
       return Promise.reject({
         body,
         status,
-      });
-    });
+      })
+    })
   }
 }
 
-async function getErrorBody(response, contentType = 'text') {
-  let body;
-  if (contentType.includes('application/json')) {
-    body = await response.json();
+async function getErrorBody(response, contentType = `text`) {
+  let body
+  if (contentType.includes(`application/json`)) {
+    body = await response.json()
   } else {
-    body = await response.text();
+    body = await response.text()
   }
-  return body;
+  return body
 }
 
-module.exports = callApi;
+module.exports = callApi
